fd343ce0be794fc540899707be1f021bf966cb80
Jeremy Lopez <26842572+lopez86@users.noreply.github.com>
2019-03-16 01:39:37 -0400

    Disabling mypy for now since protos are making it generate lots of errors.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,4 +3,4 @@
 flake8 --exclude=chupacabra_server/protos/*.py --max-line-length 99 chupacabra_server
 
 # Still need a bunch of work to get options right
-mypy --ignore-missing-imports --disallow-incomplete-defs chupacabra_server/.
+#mypy --ignore-missing-imports --disallow-incomplete-defs chupacabra_server/.
