1dcf3cd104e30c25ffad3b28f7899bcfc90ee9ac
Jeremy Lopez <26842572+lopez86@users.noreply.github.com>
2019-03-23 21:54:42 -0400

    Making the authentication postgres handler actually work

--- chupacabra_server/dbs/authentication.py
+++ chupacabra_server/dbs/authentication.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from enum import IntEnum
+from typing import NamedTuple, Optional, Tuple
 
 import arrow
 import numpy as np
@@ -13,13 +14,27 @@ username='{}' OR
 email='{}'
 """
 
+
+class CheckUserIndices(IntEnum):
+    """Tuple indices for the associated query"""
+
+    USERNAME = 0
+    EMAIL = 1
+
+
 CHECK_USER_ID_QUERY = """
 SELECT user_id
 FROM user_auth
 WHERE
 user_id='{}'
-
 """
+
+
+class CheckUserIdIndices(IntEnum):
+    """Tuple indices for the associated query"""
+
+    ID = 0
+
 
 NEW_USER_QUERY = """
 INSERT INTO user_auth
@@ -28,12 +43,23 @@ VALUES
 ('{}', '{}', '{}', '{}', '{}')
 """
 
+
 CHECK_USER_AUTH_QUERY = """
-SELECT username, nickname, user_id
+SELECT user_id, username, nickname, email
 FROM user_auth
 WHERE
 username='{}' AND password_hash='{}'
 """
+
+
+class CheckUserAuthIndices(IntEnum):
+    """Tuple indices for the associated query"""
+
+    ID = 0
+    USERNAME = 1
+    NICKNAME = 2
+    EMAIL = 3
+
 
 UPDATE_PASSWORD_QUERY = """
 UPDATE user_auth
@@ -42,9 +68,9 @@ SET password_hash='{}' WHERE user_id='{}'
 
 CREATE_TABLE_QUERY = """
 CREATE TABLE IF NOT EXISTS user_auth(
-    user_id text PRIMARY_KEY,
-    username text UNIQUE NOT NULL,
-    email text UNIQUE NOT NULL,
+    user_id text PRIMARY KEY,
+    username text NOT NULL UNIQUE,
+    email text NOT NULL UNIQUE,
     nickname text NOT NULL,
     password_hash text NOT NULL
   )
@@ -52,6 +78,15 @@ CREATE TABLE IF NOT EXISTS user_auth(
 
 MAX_USER_ID_ATTEMPTS = 5
 RANDOMIZATION_SCALE = 1000000000
+
+
+class UserAuthData(NamedTuple):
+    """User authentication data"""
+
+    user_id: str
+    username: str
+    nickname: str
+    email: str
 
 
 class AuthenticationServerHandler:
@@ -69,7 +104,6 @@ class AuthenticationServerHandler:
     def create_table(self) -> None:
         self._engine.execute(CREATE_TABLE_QUERY)
 
-
     def add_new_user(
         self,
         username: str,
@@ -79,11 +113,11 @@ class AuthenticationServerHandler:
     ) -> Tuple[bool, str]:
         results = self._engine.execute(
             CHECK_USER_QUERY.format(username, email)
-        )
+        ).fetchall()
         for result in results:
-            if result['username'] == username:
+            if result[CheckUserIndices.USERNAME.value] == username:
                 return False, 'This username has already been chosen.'
-            if result['email'] == email:
+            if result[CheckUserIndices.EMAIL.value] == email:
                 return False, 'This email has already been used.'
 
         user_id = ''
@@ -93,7 +127,7 @@ class AuthenticationServerHandler:
             proposed_user_id = str(random_state.randint(RANDOMIZATION_SCALE))
             results = self._engine.execute(
                 CHECK_USER_ID_QUERY.format(proposed_user_id)
-            )
+            ).fetchone()
             if not results:
                 user_id = proposed_user_id
                 break
@@ -102,8 +136,24 @@ class AuthenticationServerHandler:
             return False, 'Could not create a new user. Please try again.'
 
         self._engine.execute(
-            NEW_USER_QUERY.format(username, email, nickname, user_id, password_hash)
+            NEW_USER_QUERY.format(username, email, user_id, nickname, password_hash)
         )
 
         return True, 'Success'
 
+    def check_user(self, username: str, password_hash: str) -> Optional[UserAuthData]:
+        user_data = self._engine.execute(
+            CHECK_USER_AUTH_QUERY.format(username, password_hash)
+        ).fetchone()
+        if not user_data != 1:
+            return None
+
+        print(user_data)
+
+        user_auth_data = UserAuthData(
+            user_id=user_data[CheckUserAuthIndices.ID.value],
+            username=user_data[CheckUserAuthIndices.USERNAME.value],
+            nickname=user_data[CheckUserAuthIndices.NICKNAME.value],
+            email=user_data[CheckUserAuthIndices.EMAIL.value]
+        )
+        return user_auth_data
